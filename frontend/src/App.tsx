6c065f230b1a1d8d5db04af199947e7874fde028
Kliment Sokolov <kliment.sokolov@autobahn-security.com>
2024-10-20 12:54:51 +0200

    Initialize multistep pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import MainLayout from "./layouts/MainLayout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Welcome from "./pages/Welcome";
+import Name from "./pages/steps/Name";
+import Summary from "./pages/steps/Summary";
+import Email from "./pages/steps/Email";
 import NotFound from "./pages/NotFound";
 import Success from "./pages/Success";
+import Income from "./pages/steps/Income";
+import PhoneNumber from "./pages/steps/PhoneNumber";
 
 function App() {
   return (
@@ -13,6 +18,14 @@ function App() {
             <Route index element={<Welcome />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/success" element={<Success />} />
+
+            <Route path="/profile">
+              <Route path="name" element={<Name />} />
+              <Route path="email" element={<Email />} />
+              <Route path="phone" element={<PhoneNumber />} />
+              <Route path="income" element={<Income />} />
+              <Route path="summary" element={<Summary />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
