5de289c53479397d7fa5e0a1409e712304476226
Kliment Sokolov <kliment.sokolov@autobahn-security.com>
2024-10-20 16:40:41 +0200

    Create Income page

--- frontend/src/components/StepContainer.tsx
+++ frontend/src/components/StepContainer.tsx
@@ -1,4 +1,3 @@
-// StepContainer.tsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useProfileStore from "../stores/useProfileStore";
@@ -7,33 +6,35 @@ interface StepContainerProps {
   keyProp: string;
   title: string;
   defaultValue: string;
-  placeholder: string;
   inputType: string;
-  validationFn: (input: string) => string;
+  options?: string[];
+  validationFn: (value: string) => string;
   nextPath: string;
+  placeholder?: string; // Optional for text inputs
 }
 
 const StepContainer = ({
   keyProp,
   title,
   defaultValue,
-  placeholder,
   inputType,
+  options,
   validationFn,
   nextPath,
+  placeholder,
 }: StepContainerProps) => {
   const navigate = useNavigate();
   const { updateProfile } = useProfileStore();
-  const [inputValue, setInputValue] = useState(defaultValue);
+  const [value, setValue] = useState(defaultValue);
   const [error, setError] = useState("");
 
-  const validateInput = () => {
-    const errorMessage = validationFn(inputValue);
+  const handleNext = () => {
+    const errorMessage = validationFn(value);
     if (errorMessage) {
       setError(errorMessage);
     } else {
       setError("");
-      updateProfile({ [keyProp]: inputValue });
+      updateProfile({ [keyProp]: value });
       navigate(nextPath);
     }
   };
@@ -44,21 +45,38 @@ const StepContainer = ({
         <div className="flex flex-col">
           <h1 className="text-2xl font-semibold text-gray-800 mb-4">{title}</h1>
 
-          <div className="flex flex-col">
-            <input
-              type={inputType}
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              placeholder={placeholder}
-              autoFocus
-              className="p-3 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
-            />
+          <div className="flex flex-col mb-4">
+            {inputType === "radio" ? (
+              options?.map((option, index) => (
+                <label key={index} className="flex items-center mb-2">
+                  <input
+                    type="radio"
+                    name={keyProp}
+                    value={option}
+                    checked={value === option}
+                    onChange={() => setValue(option)}
+                    className="mr-2"
+                  />
+                  {option}
+                </label>
+              ))
+            ) : (
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  placeholder={placeholder}
+                  className="p-3 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                />
+              </div>
+            )}
             {error && <p className="text-red-500 text-sm">{error}</p>}
           </div>
 
           <div className="flex justify-end mt-4">
             <button
-              onClick={validateInput}
+              onClick={handleNext}
               className="bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold py-2 px-8 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-purple-300"
             >
               Next
--- frontend/src/pages/steps/Income.tsx
+++ frontend/src/pages/steps/Income.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import StepContainer from "../../components/StepContainer";
+import useProfileStore from "../../stores/useProfileStore";
 
-const Income = () => {
+const Salary = () => {
+  const { profile } = useProfileStore();
+
+  const salaryOptions = [
+    "0 - 1.000",
+    "1.000 - 2.000",
+    "2.000 - 3.000",
+    "3.000 - 4.000",
+    "More than 4.000",
+  ];
+
+  const validateSalary = (selectedSalary: string) => {
+    return selectedSalary ? "" : "Please select your income range";
+  };
+
   return (
-    <div>Income</div>
-  )
-}
+    <StepContainer
+      keyProp="income"
+      title="Please indicate your income range"
+      defaultValue={profile.income}
+      inputType="radio"
+      options={salaryOptions}
+      validationFn={validateSalary}
+      nextPath="/profile/summary/"
+    />
+  );
+};
 
-export default Income
+export default Salary;
