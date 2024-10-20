81d7011b97ff2caa92371c99f728c4d15f451c02
Kliment Sokolov <kliment.sokolov@autobahn-security.com>
2024-10-20 13:07:28 +0200

    Set Header with Logo

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import logo from "../assets/icons/buena-logo.svg";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <div className="h-20">
+      <nav className="flex px-5 py-5 items-center justify-between">
+        <Link to="/">
+          <img className="h-6" src={logo} alt="Buena" />
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Header
